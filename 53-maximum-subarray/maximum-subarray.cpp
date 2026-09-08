e121eed8c5447d7041523ccf430783615123d8d0
kunalwadhwa007 <kunalwadhwa2006@gmail.com>
2026-09-08 11:58:44 +0530

    Time: 2 ms (21.35%) | Memory: 71.7 MB (52.52%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = 0;
+        int maxi = INT_MIN;
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+            curr += nums[i];
+            maxi = max(maxi,curr);
+            if(curr<0){
+                curr=0;
+            }
+
+        }
+        return maxi;
+        
+    }
+};
